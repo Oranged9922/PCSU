350814bfc2333da24b3536da9d4dd452cb66189b
Lukáš Salak <lukas.salak1@gmail.com>
2022-09-19 22:15:38 +0200

    Added new OnDeleteWindow for selecting and removing photos from working repository

---
+++ source/PCSU.MainApplication/OnDeleteWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace PCSU.MainApplication
+{
+	using System.Windows;
+	using PCSU.Controllers;
+
+	/// <summary>
+	/// Interaction logic for OnDeleteWindow.xaml
+	/// </summary>
+	public partial class OnDeleteWindow : Window
+	{
+		private readonly PhotoController _photoController;
+
+		public OnDeleteWindow(PhotoController photoController)
+		{
+			InitializeComponent();
+			_photoController = photoController;
+			_photoController.GetAllPhotos().ForEach(x => this.ListBox.Items.Add(x));
+		}
+	}
+}
